be34c5b80d898b253fc15951dd026d2727163113
Johan Joseph <iamstevefromspace@gmail.com>
2025-09-19 19:09:19 +0530

    Fixed URL Error

--- SFS New Website/app.py
+++ SFS New Website/app.py
@@ -70,7 +70,7 @@ def get_media_files():
 def index():
     return send_from_directory('.', 'index.html')
 
-@app.route('/<path/filename>')
+@app.route('/<path:filename>')
 def serve_static(filename):
     return send_from_directory('.', filename)
 
